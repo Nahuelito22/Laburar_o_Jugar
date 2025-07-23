928bf3ba786afea41746feb07ee1e5ae9979b7bc
Nahuel <matiasghilardisalinas@gmail.com>
2025-07-23 00:46:03 -0300

    Configuraciones basicas y necesarias en main.py y settins.py -  Estas configuraciones corren el juego y cargan parametros de pantalla,fps,etc

---
+++ main.py
@@ -0,0 +1,6 @@
+# main.py
+from src.app import App
+
+if __name__ == "__main__":
+    game_app = App()
+    game_app.run()
---
+++ src/settings.py
@@ -0,0 +1,15 @@
+# src/settings.py
+import pygame
+
+# Dimensiones de la pantalla
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SCREEN_SIZE = (SCREEN_WIDTH, SCREEN_HEIGHT)
+
+# Fotogramas por segundo
+FPS = 60
+
+# Colores (en formato RGB)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
